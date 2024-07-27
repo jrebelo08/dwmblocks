6ef6657f79b6d6537e7afea3cbab1282d00bc815
rebelojoao <rebelojoao@drb.lan>
2024-07-27 14:42:44 +0100

    My custom dwmblocks build

--- blocks.h
+++ blocks.h
@@ -1,18 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-    /*Icon*/    /*Command*/     /*Update Interval*/ /*Update Signal*/
-    // {" ",      "coinmon -f XMR | grep 'XMR' | awk '{print $6}'",
-    //                             120,                0},
-    {" ",      "nmcli -f IN-USE,SIGNAL,SSID device wifi | grep \"*\" | awk '{print $3 \": \" $2}'",
-                                30,                 0},
-    {" ",      "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",  
-                                30,                 0},
+    /*Icon*/    /*Command*/                             /*Update Interval*/ /*Update Signal*/
+    {"",        "~/.local/bin/kb.sh",   
+                                300,                 0},
+    {"",        "~/.local/bin/bluetooth.sh",   
+                                5,                 0},
+    {" ",      "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",  
+                                5,                 0},
+    {"",        "~/.local/bin/battery.sh",   
+                                5,                 0},
     {" ",      "date '+%a %b %d  %H:%M'",                               
-                                5,                  0},
-    {"",        "block_battery",   
-                                10,                 0},
+                                60,                  0},
+    {"",        "~/.local/bin/volume.sh",   
+                                1,                 0},
 };
-
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = "  ";
 static unsigned int delimLen = 5;
